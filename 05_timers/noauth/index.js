1c04eefba9f3586b101062a602ae5fd569bb56ad
andriimsn <andrewmosin8@gmail.com>
2021-08-23 18:46:39 +0300

    05 / timer / noauth

--- 05_timers/noauth/index.js
+++ 05_timers/noauth/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
-// const { nanoid } = require("nanoid");
+const { nanoid } = require("nanoid");
 
 const app = express();
 
@@ -26,6 +26,7 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+let TIMERS = [];
 // You can use these initial data
 /*
 const TIMERS = [
@@ -45,6 +46,50 @@ const TIMERS = [
   },
 ];
 */
+
+app.get("/api/timers", async (req, res) => {
+  const { isActive } = await req.query;
+
+  let timers = TIMERS.filter((el) => el.isActive.toString() === isActive).map((el) => {
+    if (el.isActive) {
+      el.progress = Date.now() - el.start;
+    }
+    return el;
+  });
+  if (timers.length > 0) {
+    return res.status(200).json(timers);
+  }
+  return res.status(204).end();
+});
+
+app.post("/api/timers", (req, res) => {
+  const { description } = req.body;
+  const id = nanoid();
+  const timer = {
+    start: Date.now(),
+    description,
+    isActive: true,
+    id,
+  };
+  TIMERS.push(timer);
+  console.log(TIMERS);
+  return res.status(200).json({ id });
+});
+
+app.post("/api/timers/:id/stop", (req, res) => {
+  const id = req.params.id;
+  TIMERS = TIMERS.map((el) => {
+    if (el.id === id) {
+      delete el.progress;
+      el.end = Date.now();
+      el.duration = el.end - el.start;
+      el.isActive = false;
+    }
+    return el;
+  });
+  console.log(TIMERS);
+  return res.status(200).json({ id });
+});
 
 const port = process.env.PORT || 3000;
 
